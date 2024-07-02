874278a1fc94334fc078dc4b9052e8dd9a484e4d
Marcelo P Duarte <marcelopd20@gmail.com>
2024-07-02 15:10:07 -0300

    I've started a new repository to develop the challenge project

---
+++ NodeLightningChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NodeLightningChallenge
+//
+//  Created by Marcelo Pastana Duarte on 02/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NodeLightningChallenge/NodeLightningChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodeLightningChallengeApp.swift
+//  NodeLightningChallenge
+//
+//  Created by Marcelo Pastana Duarte on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NodeLightningChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
