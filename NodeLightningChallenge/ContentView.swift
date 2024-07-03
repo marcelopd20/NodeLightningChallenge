303333ba440114dfa753fceace6ae12cf3db1e37
Marcelo P Duarte <marcelopd20@gmail.com>
2024-07-02 22:19:47 -0300

    Develop branch ready to start

--- NodeLightningChallenge/ContentView.swift
+++ NodeLightningChallenge/ContentView.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+// TODO: Criar model para json de node
+// TODO: Criar função para conectar na api - https://mempool.space/api/v1/lightning/nodes/rankings/connectivity , unix time, pt-br senão en,
+// TODO: Criar tratamento de exceções da api
+// TODO: Criar testes para função e verificar erros atraves dos testes
+// TODO: Desenvolver na UI - Intuitiva, dinâmica, metodo para atualizar dados
+// TODO: Tratar dados para apresentar na UI 1 BTC = 1e8 sats
+// TODO: Exibir estado de loading
+// TODO: Testar
+// TODO: Readme
+
 struct ContentView: View {
     var body: some View {
         VStack {
